86e12622e3d0f38219b6153b27742655cd9aa669
kalgi <kalgi@nevercode.io>
2022-02-17 11:34:53 +0530

    Initial Commit

---
+++ My First iOS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First iOS App
+//
+//  Created by Kalgi Shah on 17/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My First iOS App/My_First_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_iOS_AppApp.swift
+//  My First iOS App
+//
+//  Created by Kalgi Shah on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct My_First_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
